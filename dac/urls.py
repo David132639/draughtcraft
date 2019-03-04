a0c7ebb752379582022145bc71f65dea3383def5
myles lamb <mylesalamb@gmail.com>
2019-03-04 15:23:58 +0000

    url mappings with placeholder views implemented

--- dac/urls.py
+++ dac/urls.py
@@ -1,6 +1,32 @@
 from django.conf.urls import url
 from dac import views
 
+
+'''List reviews and pubs subject to change'''
+
 urlpatterns = [
+
+#beers
 url(r'^$', views.index, name='index'),
+url(r'^beers/$', views.beers, name='beers'),
+url(r'^beers/(?P<beer_slug>[\w\-]+)/$', views.beers, name='beers'),
+url(r'^beers/(?P<beer_slug>[\w\-]+)/reviews$', views.beers_reviews, name='beers'),
+url(r'^beers/(?P<beer_slug>[\w\-]+)/add_review$', views.add_beer_review, name='beers'),
+
+
+#pubs
+url(r'^pubs/$', views.pubs, name='pubs'),
+url(r'^pubs/(?P<pub_slug>[\w\-]+)/$', views.pubs, name='pubs'),
+url(r'^pubs/(?P<pub_slug>[\w\-]+)/beers/$', views.pubs_beers, name='pubs'),
+
+#user account stuff
+url(r'^search/(?P<query_string>[\w\-]+)/$',views.search,name='search'),
+url(r'^sitemap/$',views.sitemap,name='sitemap'),
+
+#accounts
+url(r'^account/login/$', views.login, name='login'),
+url(r'^account/logout/$', views.logout, name='logout'),
+url(r'^account/register/$', views.register, name='register'),
+url(r'^account/reviews/$', views.user_reviews, name='reviews'),
+url(r'^restricted/$', views.restricted, name='restricted'),
 ]
--- dac/views.py
+++ dac/views.py
@@ -1,6 +1,89 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# General site pages
 def index(request):
+	context_dict = {}
 	return HttpResponse("test")
+
+def sitemap(request):
+	return HttpResponse("A sitemap")
+
+
+#beer stuff
+def beers(request,beer_slug=None):
+	context_dict = {}
+
+	if not beer_slug:
+		return HttpResponse("where beers will be listed")
+	print(beer_slug)
+	return HttpResponse("a specific beer called "+beer_slug)
+
+def add_beer_review(request,beer_slug):
+	#basic form for user adding review to a specific beer
+	return HttpResponse("not implemented")
+	pass
+
+
+def beers_reviews(request,beer_slug):
+	context_dict = {}
+	#get the reviews associated with the beer and return the stuff
+	#use same template as pub reviews
+	return HttpResponse("not implemented")
+	pass
+
+
+#pub stuff
+
+def pubs(request,pub_slug=None):
+	context_dict = {}
+
+	if not pub_slug:
+		return HttpResponse("print a whole load of pubs")
+	return HttpResponse("a specific pub"+pub_slug)
+
+def pubs_beers(request,pub_slug):
+	#return a list of the beers that the pub stocks
+	return HttpResponse("not implemented")
+	pass
+
+
+def pub_reviews(request,pub_slug):
+	context_dict = {}
+
+	#get the reviews associated with the pub and return
+	#use same template as pub reviews
+	return HttpResponse("not implemented yet")
+
+
+#searching stuff
+
+def search(request):
+	HttpResponse("not implemented")
+	pass
+
+#account functionality
+
+@login_required
+def user_reviews(request):
+	context_dict = {}
+	HttpResponse("not implemented")
+	pass
+
+def login(request):
+	HttpResponse("not implemented")
+	pass
+
+@login_required
+def logout(request):
+	HttpResponse("not implemented")
+	pass
+
+def register(request):
+	HttpResponse("not implemented")
+	pass
+
+def restricted(request):
+	HttpResponse("Access Restricted")
+	pass
