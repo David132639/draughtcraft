a3230263ea39db4d443604e2c86a2e367dc3d6be
myles lamb <mylesalamb@gmail.com>
2019-03-18 17:16:45 +0000

    slight amendment so project will work

--- wad_team_project/settings.py
+++ wad_team_project/settings.py
@@ -34,8 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 #AUTOCOMPLETE LIGHT MUST BE FIRST
 INSTALLED_APPS = [
-    'dal',
-    'dal_select2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
