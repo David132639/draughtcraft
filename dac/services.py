b4f4e99acd3e6aeac609783ccb7f352e044a9a03
myles lamb <mylesalamb@gmail.com>
2019-03-21 15:10:00 +0000

    adjusting registration form and pub page

--- dac/services.py
+++ dac/services.py
@@ -4,13 +4,11 @@ from django.conf import settings
 GOOGLE_MAPS_FIELDS = ["geometry","name","formatted_address","place_id",]
 PLACE_FIELDS = ["photo",]
 
-GOOGLE_PLACES_API_KEY = 'AIzaSyDhirIPKrHsY6xXlRXMHFehpFEflNAqrEU'
-
 def get_image_reference(place_id,client=None):
     '''returns a reference object to the first image returned from teh places api
     or none if some error is encountered'''
     if not client:
-        client = googlemaps.Client(key=GOOGLE_PLACES_API_KEY)
+        client = googlemaps.Client(key=settings.GOOGLE_PLACES_API_KEY)
 
     place_results = client.place(place_id,fields=PLACE_FIELDS)
     
@@ -24,9 +22,9 @@ def get_image_from_reference(reference,filename,client=None):
     print(reference," ",filename)
 
     if not client:
-        client = googlemaps.Client(key=GOOGLE_PLACES_API_KEY)
+        client = googlemaps.Client(key=settings.GOOGLE_PLACES_API_KEY)
 
-    
+    '''Small file size to try and prevent throttling by google'''
     with open(filename,"wb") as outfile:
         for chunk in client.places_photo(
             reference["photo_reference"],max_width=175,max_height=175):
@@ -38,8 +36,9 @@ def get_image_from_reference(reference,filename,client=None):
 
 
 def get_place_info(address):
-    #replace with settings.GOOGLE.....
-    gmaps = googlemaps.Client(key=GOOGLE_PLACES_API_KEY)
+    '''Returns basic fields about a given business from the given address
+    or none if no results are returned from the api'''
+    gmaps = googlemaps.Client(key=setting.GOOGLE_PLACES_API_KEY)
 
     place_results = gmaps.find_place(address,input_type="textquery",fields=GOOGLE_MAPS_FIELDS)
 
