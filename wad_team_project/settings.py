7a89c5bcca0a188bbdffc9b8c60eae1f2e44adb6
Marcell Pek <marcellpek0@gmail.com>
2019-03-21 21:32:56 +0000

    Change API key

--- wad_team_project/settings.py
+++ wad_team_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_-dk-4*ta+2r!)_k^dlxm)#%7a#u6a7s6&#943)$qi+!0uetey'
 
 # Google Places API key
-GOOGLE_PLACES_API_KEY = 'AIzaSyDhirIPKrHsY6xXlRXMHFehpFEflNAqrEU'
+GOOGLE_PLACES_API_KEY = 'AIzaSyAJBlNwBaml17vY8P1SUzLrtyghzq1K3OQ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
