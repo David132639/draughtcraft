06434355ee914ec62449f234566dfdd267544ae4
myles lamb <mylesalamb@gmail.com>
2019-03-21 21:02:29 +0000

    read more doesnt push page, index page scaling now fixed, search results scaling fixed

--- static/js/reading.js
+++ static/js/reading.js
@@ -5,12 +5,14 @@ content are nested inside a hidden class
 where it is the next element
 */
 $(document).ready(function(){
-	$('.read-more').on('click',function(){
+	$('.read-more').on('click',function(e){
+		e.preventDefault()
 		$(this).addClass('hide')
 		$(this).next().removeClass('hide');
 	})
 
-	$('.read-less').on('click',function(){
+	$('.read-less').on('click',function(e){
+		e.preventDefault()
 		$(this).parent().addClass('hide')
 		$(this).parent().prev().removeClass('hide')
 
